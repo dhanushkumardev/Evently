938e8d99014a23a0870b154bf89160b4f8282134
Dhanush Kumar <dhanushkumar68202@gmail.com>
2024-10-07 17:20:50 +0530

    3rd commit

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -3,7 +3,7 @@ import { headers } from "next/headers";
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { createUser, deleteUser, updateUser } from "@/lib/actions/user.actions";
 import { NextResponse } from "next/server";
-import { clerkClient } from '@clerk/clerk-sdk-node'
+import { clerkClient } from "@clerk/clerk-sdk-node";
 
 export async function POST(req: Request) {
     // You can find this in the Clerk Dashboard -> Webhooks -> choose the endpoint
